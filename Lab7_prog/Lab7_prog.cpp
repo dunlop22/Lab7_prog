fb3e3510890129d4de6545b8e01afd8099cab94a
Egor Golikov <20020110ll@gmail.com>
2021-11-11 15:35:27 +0700

    Задание №1 Продемонстрировать возврат значения из метода через указатель (*) и через ссылку (&).

--- Lab7_prog/Lab7_prog.cpp
+++ Lab7_prog/Lab7_prog.cpp
@@ -22,87 +22,103 @@ int main()
     Korobka Korobas;
     do
     {
-        system("cls");
-        cout << "1) Добавление информации о колесах\n2) Добавление информации о коробке передач\n3) Добавление информации о двигателе\n4) Добавление общей информации\n5) Создание авто\n\n0) Выход";
-        menu = _getch();
-    } while (menu < '0' && menu > '5');
-    if (menu == '1')
-    {
         do
         {
             system("cls");
-            cout << "1) Создание колеса со своими данными\n2) Создание колеса по конструктору";
-            podmenu = _getch();
-        } while (podmenu < '1' || podmenu > '2');
-        if (podmenu == '1')
+            cout << "1) Добавление информации о колесах\n2) Добавление информации о коробке передач\n3) Добавление информации о двигателе\n4) Добавление общей информации\n5) Создание авто\n6) Возврат значений (* | &)\n\n0) Выход";
+            menu = _getch();
+        } while (menu < '0' && menu > '6');
+        if (menu == '1')
         {
-            Kolesiko.new_koleso_info();
+            do
+            {
+                system("cls");
+                cout << "1) Создание колеса со своими данными\n2) Создание колеса по конструктору";
+                podmenu = _getch();
+            } while (podmenu < '1' || podmenu > '2');
+            if (podmenu == '1')
+            {
+                Kolesiko.new_koleso_info();
+            }
+            else
+            {
+                Kolesiko.new_koleso(225, 55, 17, "Литье");
+            }
+            Kolesiko.prosmotr_koleso();
         }
-        else
+        if (menu == '2')
         {
+            do
+            {
+                system("cls");
+                cout << "1) Создание коробки со своими данными\n2) Создание коробки по конструктору";
+                podmenu = _getch();
+            } while (podmenu < '1' || podmenu > '2');
+            if (podmenu == '1')
+            {
+                Korobas.new_korobka_info();
+            }
+            else
+            {
+                Korobas.new_korobka_peredach(5, "АКПП");
+            }
+            Korobas.prosmotr_korobka_peredach();
+        }
+        if (menu == '3')
+        {
+            do
+            {
+                system("cls");
+                cout << "1) Создание двигателя со своими данными\n2) Создание двигателя по конструктору";
+                podmenu = _getch();
+            } while (podmenu < 1 || podmenu > 2);
+
+            if (podmenu == '1')
+            {
+                Motorik.new_motor_info();
+            }
+            if (podmenu == '2')
+            {
+                Motorik.new_motor(100, 10, "FB20", 2, 4, 4);
+            }
+            Motorik.prosmotr_motor();
+        }
+        if (menu == '4')
+        {
+            do
+            {
+                system("cls");
+                cout << "1) Создание общей информации со своими данными\n2) Создание общей информации по конструктору";
+                podmenu = _getch();
+            } while (podmenu < 1 || podmenu > 2);
+
+            if (podmenu == '1')
+            {
+                Obchie.new_obchee_info();
+            }
+            if (podmenu == '2')
+            {
+                Obchie.new_obchee(5, 1500, "Toyota", 7, "4WD", "Бензин", 65);
+            }
+            Obchie.prosmotr_obchee();
+        }
+        if (menu == '5')
+        {
+
+        }
+        if (menu == '6')
+        {
+            system("cls");
+            double diametr_ret = 0;
+            double visota_ret = 0;
+            
             Kolesiko.new_koleso(225, 55, 17, "Литье");
+            Kolesiko.Diametr_inf(diametr_ret);
+            cout << "Возврат через ссылку\nДиаметр: " << diametr_ret;
+            Kolesiko.Visota_inf(&visota_ret);
+            cout << "\n\nВозврат через указатель\nВысота: " << visota_ret;
+            cout << "\n\n\nНажмите любую клавишу для возврата в меню";
+            _getch();
         }
-        Kolesiko.prosmotr_koleso();
-    }
-    if (menu == '2')
-    {
-        do
-        {
-            system("cls");
-            cout << "1) Создание коробки со своими данными\n2) Создание коробки по конструктору";
-            podmenu = _getch();
-        } while (podmenu < '1' || podmenu > '2');
-        if (podmenu == '1')
-        {
-            Korobas.new_korobka_info();
-        }
-        else
-        {
-            Korobas.new_korobka_peredach(5, "АКПП");
-        }
-        Korobas.prosmotr_korobka_peredach();
-    }
-    if (menu == '3')
-    {
-        do
-        {
-            system("cls");
-            cout << "1) Создание двигателя со своими данными\n2) Создание двигателя по конструктору";
-            podmenu = _getch();
-        } while (podmenu < 1 || podmenu > 2);
-
-        if (podmenu == '1')
-        {
-            Motorik.new_motor_info();
-        }
-        if (podmenu == '2')
-        {
-            Motorik.new_motor(100, 10, "FB20", 2, 4, 4);
-        }
-        Motorik.prosmotr_motor();
-    }
-    if (menu == '4')
-    {
-        do
-        {
-            system("cls");
-            cout << "1) Создание общей информации со своими данными\n2) Создание общей информации по конструктору";
-            podmenu = _getch();
-        } while (podmenu < 1 || podmenu > 2);
-
-        if (podmenu == '1')
-        {
-            Obchie.new_obchee_info();
-        }
-        if (podmenu == '2')
-        {
-            Obchie.new_obchee(5, 1500, "Toyota", 7, "4WD", "Бензин", 65);
-        }
-        Obchie.prosmotr_obchee();
-    }
-    if (menu == '5')
-    {
-
-    }
-
+    } while (menu != 0);
 }
